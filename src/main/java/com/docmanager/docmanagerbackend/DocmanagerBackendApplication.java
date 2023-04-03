d53ff970e3f1c4766d925417dff03ff27539a30e
Petrescu Robert <petrescu.robert69@gmail.com>
2023-04-03 09:42:26 +0300

    Initial commit

---
+++ src/main/java/com/docmanager/docmanagerbackend/DocmanagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.docmanager.docmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocmanagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocmanagerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docmanager/docmanagerbackend/DocmanagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docmanager.docmanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocmanagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
